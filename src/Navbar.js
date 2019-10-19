9e37e0ff22cd189c1947b72105557db21b223b9f
Milad Pashaei <milad-pn@live.com>
2019-10-19 14:26:09 +0330

    add Changeformat snackbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
@@ -9,15 +12,19 @@ import './Navbar.css';
 export class Navbar extends Component {
   constructor(props) {
     super(props);
-    this.state = { format: 'hex' };
+    this.state = { format: 'hex', open: false };
     this.handleChange = this.handleChange.bind(this);
+    this.closeSnakebar = this.closeSnakebar.bind(this);
   }
   handleChange(e) {
-    this.setState({ format: e.target.value });
+    this.setState({ format: e.target.value, open: true });
     this.props.handleChange(e.target.value);
   }
+  closeSnakebar() {
+    this.setState({ open: false });
+  }
   render() {
-    const { level, changeLevel, handleChange } = this.props;
+    const { level, changeLevel } = this.props;
     const { format } = this.state;
     return (
       <header className='Navbar'>
@@ -45,6 +52,23 @@ export class Navbar extends Component {
             <MenuItem value='rgba'>RGBA - rgba(255,255,255,1.0) </MenuItem>
           </Select>
         </div>
+        <Snackbar
+          anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+          open={this.state.open}
+          message={<span id='message-id'>Format Changed!</span>}
+          ContentProps={{ 'aria-describedby': 'message-id' }}
+          onClose={this.closeSnakebar}
+          action={[
+            <IconButton
+              onClick={this.closeSnakebar}
+              color='inherit'
+              key='close'
+              aria-label='close'
+            >
+              <CloseIcon />
+            </IconButton>
+          ]}
+        />
       </header>
     );
   }
