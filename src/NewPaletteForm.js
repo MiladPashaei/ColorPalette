3ed0be65dc9d1e14f9be8c7e7227b9c1b3d0d660
Milad Pashaei <milad.pn@outlook.com>
2019-12-22 12:25:21 +0330

    'fix some css issue'

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -14,61 +14,7 @@ import Button from "@material-ui/core/Button";
 import DraggableColorList from "./draggableColorList";
 import { arrayMove } from "react-sortable-hoc";
 import PaletteFormNav from "./PaletteFormNav";
-
-const drawerWidth = 400;
-
-const styles = theme => ({
-  root: {
-    display: "flex"
-  },
-
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0
-  },
-  drawerPaper: {
-    width: drawerWidth,
-    display: "flex",
-    alignItems: "center"
-  },
-  drawerHeader: {
-    display: "flex",
-    alignItems: "center",
-    padding: "0 8px",
-    ...theme.mixins.toolbar,
-    justifyContent: "flex-end"
-  },
-  content: {
-    height: "calc(100vh - 64px)",
-    flexGrow: 1,
-    padding: theme.spacing.unit * 3,
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    }),
-    marginLeft: -drawerWidth
-  },
-  contentShift: {
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen
-    }),
-    marginLeft: 0
-  },
-  buttons: { width: "100%" },
-  button: { width: "50%" },
-  container: {
-    width: "90%",
-    height: "100%",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  backbtn: {
-    marginRight: "12px"
-  }
-});
+import styles from "./Styles/NewPaletteForm";
 
 class NewPaletteForm extends Component {
   static defaultProps = {
